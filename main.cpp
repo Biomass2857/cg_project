1ad242faac2a78f5e7adef7049034cef864f281b
Biomass2857 <wasserfrosch1234@gmail.com>
2024-01-19 18:34:29 +0100

    add changes

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <string>
@@ -30,7 +29,7 @@ const char* fragmentShaderSource = "#version 330 core\
         FragColor = vec4(color, 1.0);\
     }";
 
-void errorCallback( int iError, const char* pcDescription) {
+void errorCallback(int iError, const char* pcDescription) {
     std::cerr << "GLFW Error: " + std::to_string(iError) + " " + std::string(pcDescription) << std::endl;
 }
 
@@ -116,116 +115,4 @@ int main() {
     glfwTerminate();
 
     return 0;
-=======
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-#include <string>
-
-#ifdef __APPLE__
-#include <OpenGL/gl.h>
-#elif __linux__
-#include <GL/gl.h>
-#elif _WIN32
-#include <GL/gl.h>
-#endif
-
-const char* vertexShaderSource = "#version 330 core\
-    layout (location = 0) in vec3 aPos;\
-    layout (location = 1) in vec3 aColor;\
-    out vec3 color;\
-    void main()\
-    { \
-        color = aColor;\
-        gl_Position = vec4(aPos, 1.0);\
-    }";
-
-const char* fragmentShaderSource = "#version 330 core\
-    in vec3 color;\
-    out vec4 FragColor;\
-    void main()\
-    {\
-        FragColor = vec4(color, 1.0);\
-    }";
-
-int main() {
-    if(!glfwInit()) {
-        return -1;
-    }
-
-    // glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    // glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Project", nullptr, nullptr);
-    if(!window) {
-        glfwTerminate();
-        return -1;
-    }
-
-    unsigned int vertexShader, fragmentShader, shaderProgram;
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
-    glCompileShader(vertexShader);
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
-    glCompileShader(fragmentShader);
-    shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
-    float vertices[] = {
-        // positions          // colors
-        -0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 0.0f, // front face
-         0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.0f,
-         0.5f,  0.5f, -0.5f,  0.0f, 0.0f, 1.0f,
-         0.5f,  0.5f, -0.5f,  0.0f, 0.0f, 1.0f, // right face
-        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 0.0f,
-        -0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 0.0f,
-         // ... (repeat for other faces)
-    };
-
-    unsigned int VAO, VBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
-
-    glfwMakeContextCurrent(window);
-
-    if(glewInit() != GLEW_OK) {
-        glfwTerminate();
-        return -1;
-    }
-
-    while(!glfwWindowShouldClose(window)) {
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
-        glBindVertexArray(0);
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
-    glDeleteProgram(shaderProgram);
-
-    glfwTerminate();
-
-    return 0;
->>>>>>> 3de46a31bb8b47142a91c2ee7c4b61f0e35452e5
 }
