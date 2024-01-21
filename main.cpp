0269e94458977fa9888bbe33bbd5f4b4756d275d
Biomass2857 <wasserfrosch1234@gmail.com>
2024-01-21 21:17:15 +0100

    fix depth test

--- main.cpp
+++ main.cpp
@@ -117,10 +117,14 @@ int main() {
     glDeleteShader(fragmentShader);
 
     float theta = 0.5f;
+
     glUseProgram(shaderProgram);
+
+    glEnable(GL_DEPTH_TEST);
+
     while(!glfwWindowShouldClose(window)) {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         theta += 0.01f;
         float rotMatrix[16] = {
