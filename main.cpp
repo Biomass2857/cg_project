d915a97547fb5fb9ef5fd6a8c58e80fe8c9cfece
Biomass2857 <wasserfrosch1234@gmail.com>
2024-01-21 21:07:14 +0100

    add comments

--- main.cpp
+++ main.cpp
@@ -70,18 +70,22 @@ int main() {
         len, -len, len, 0.0f, 1.0f, 0.0f
     };
 
-
-
     unsigned int VAO, VBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // read positions from buffer
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+
+    // read colors from buffer
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    // enable vertex attribute number 1 (we use as color)
     glEnableVertexAttribArray(1);
+
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
