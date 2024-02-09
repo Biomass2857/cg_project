47d602729b62f9c2e3a2d894af76572cc2e3acab
Pixelbuddha <sefisch@googlemail.com>
2024-02-09 18:55:40 +0100

    Aufraeumen und ordner

---
+++ hpp_cpp/camera.hpp
@@ -0,0 +1,17 @@
+#ifndef CAMERA_CLASS_H
+#define CAMERA_CLASS_H
+
+#include <GL/glew.h>
+#include<GLFW/glfw3.h>
+#include<glm/glm.hpp>
+#include<glm/gtc/matrix_transform.hpp>
+#include<glm/gtc/type_ptr.hpp>
+#include<glm/gtx/rotate_vector.hpp>
+#include<glm/gtx/vector_angle.hpp>
+
+class Camera
+{
+public:
+
+};
+#endif
